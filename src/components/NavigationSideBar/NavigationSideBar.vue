bf8a5fd49ad4e595f41e3103dc7b2ac0a98240ac
JardaFiser <fiser@cngroup.dk>
2022-08-15 14:51:30 +0200

    vue-cli component import test

---
+++ src/components/NavigationSideBar/NavigationSideBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts" src="./NavigationSideBar.ts">
+</script>
+
+<style scoped src="./NavigationSideBar.css">
+</style>
